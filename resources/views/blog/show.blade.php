34d594a613c0ca71629f92c9b878bd2a644289bf
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-09 14:18:32 +0700

    Create blog resource view

---
+++ resources/views/blog/show.blade.php
@@ -0,0 +1,145 @@
+@extends('blog.layout.master')
+
+@section('content')
+  <!-- banner -->
+  	<div class="banner1">
+  		<div class="container">
+  			<div class="w3ls_logo w3ls_logo_about">
+  				<h1><a href="index.html">Cattle Farm</a></h1>
+  			</div>
+  		</div>
+  	</div>
+  <!-- //banner -->
+  <!-- breadcrumbs -->
+  	<div class="agileits_breadcrumbs">
+  		<div class="container">
+  			<div class="agileits_breadcrumbs_left">
+  				<ul>
+  					<li><a href="index.html">Home</a><i>|</i></li>
+  					<li><a href="blog.html">Blog</a> <i>&raquo;</i> Single</li>
+  				</ul>
+  			</div>
+  			<div class="agileits_breadcrumbs_right">
+  				<h2>Single</h2>
+  			</div>
+  			<div class="clearfix"> </div>
+  		</div>
+  	</div>
+  <!-- //breadcrumbs -->
+  <!-- single -->
+  	<div class="blog">
+  		<div class="container">
+  			<div class="col-md-7 wthree_blog_left">
+  				<div class="wthree_blog_left_grid">
+  					<div class="wthree_blog_left_grid_slider">
+  						<img src="images/12.jpg" alt=" " class="img-responsive" />
+  					</div>
+  					<h4>Cattle Farm</h4>
+  					<h3>facilisis velit vitae gravida imperdiet</h3>
+  					<ul>
+  						<li><span class="glyphicon glyphicon-user" aria-hidden="true"></span><a href="#">Jan Mark</a><i>|</i></li>
+  						<li><span class="glyphicon glyphicon-heart" aria-hidden="true"></span><a href="#">20</a><i>|</i></li>
+  						<li><span class="glyphicon glyphicon-tag" aria-hidden="true"></span><a href="#">5</a><i>|</i></li>
+  						<li><span class="glyphicon glyphicon-edit" aria-hidden="true"></span>Voluptatibus</li>
+  					</ul>
+  					<p>Duis venenatis, mi quis malesuada ultricies, nisi nisl posuere sapien,
+  						id egestas lectus urna et massa. Morbi mauris turpis, posuere auctor auctor
+  						ornare, auctor nec lectus. Class aptent taciti sociosqu ad litora torquent
+  						per conubia nostra, per inceptos himenaeos.
+  						<i>Nulla faucibus, arcu at consequat vulputate, turpis metus blandit urna,
+  						quis suscipit orci nibh nec est. Phasellus tempus augue turpis. Fusce nec
+  						tellus blandit, ullamcorper sapien vel, placerat erat. Nam non massa elit.
+  						Morbi placerat molestie felis. Integer facilisis velit leo, vitae gravida
+  						elit fringilla nec.</i>Quisque id nunc nec risus molestie aliquam.
+  						Donec ex eros, lobortis et lorem quis, sagittis malesuada lectus.
+  						Suspendisse ornare sed purus a imperdiet. Integer viverra nisi orci,
+  						in volutpat leo ornare vitae.</p>
+  				</div>
+  				<div class="agileits_share">
+  					<ul>
+  						<li><a href="#" class="facebook1"></a></li>
+  						<li><a href="#" class="twitter1"></a></li>
+  						<li><a href="#" class="google1"></a></li>
+  						<li><a href="#" class="instagram1"></a></li>
+  					</ul>
+  				</div>
+  				<div class="agileits_reply">
+  					<h3>Leave a reply</h3>
+  					<form action="#" method="post">
+  						<input type="text" name="Name" placeholder="Name" required="">
+  						<input type="email" name="Email" placeholder="Email" required="">
+  						<input type="text" name="Subject" placeholder="Subject" required="">
+  						<textarea name="Message" placeholder="Type your comment..." required=""></textarea>
+  						<input type="submit" value="Submit">
+  					</form>
+  				</div>
+  			</div>
+  			<div class="col-md-5 wthree_blog_right">
+  				<div class="w3ls_search">
+  					<form action="#" method="post">
+  						<input type="text" name="Search" placeholder="Search on our blog" required="">
+  						<input type="submit" value=" ">
+  					</form>
+  				</div>
+  				<div class="w3l_categories">
+  					<h3>Categories</h3>
+  					<ul>
+  						<li><a href="single.html">Vitae gravida elit fringilla nec sociosqu</a></li>
+  						<li><a href="single.html">Morbi mauris turpis, posuere auctor auctor</a></li>
+  						<li><a href="single.html">Class aptent taciti sociosqu litora torquent</a></li>
+  						<li><a href="single.html">Duis venenatis, mi quis malesuada ultricies</a></li>
+  						<li><a href="single.html">Egestas lectus urna et massa morbi mauris</a></li>
+  						<li><a href="single.html">Torquent conubia nostra inceptos himenaeos</a></li>
+  						<li><a href="single.html">Urna et massa morbi mauris turpis, posuere</a></li>
+  					</ul>
+  				</div>
+  				<div class="agileinfo_tags">
+  					<h3>Tags</h3>
+  					<ul>
+  						<li><a href="single.html">Milk</a></li>
+  						<li><a href="single.html">Feed</a></li>
+  						<li><a href="single.html">Agriculture</a></li>
+  						<li><a href="single.html">Travel</a></li>
+  						<li><a href="single.html">Products</a></li>
+  						<li><a href="single.html">Health</a></li>
+  						<li><a href="single.html">Grass</a></li>
+  					</ul>
+  				</div>
+  				<div class="w3l_archives">
+  					<h3>Archives</h3>
+  					<ul>
+  						<li>2 August 2016</li>
+  						<li>14 August 2016</li>
+  						<li>23 August 2016</li>
+  						<li>8 September 2016</li>
+  						<li>18 September 2016</li>
+  					</ul>
+  				</div>
+  				<div class="w3agile_flickr_posts">
+  					<h3>Flickr Posts</h3>
+  					<div class="w3agile_flickr_post_left">
+  						<a href="single.html"><img src="images/1.jpg" alt=" " class="img-responsive" /></a>
+  					</div>
+  					<div class="w3agile_flickr_post_left">
+  						<a href="single.html"><img src="images/4.jpg" alt=" " class="img-responsive" /></a>
+  					</div>
+  					<div class="w3agile_flickr_post_left">
+  						<a href="single.html"><img src="images/3.jpg" alt=" " class="img-responsive" /></a>
+  					</div>
+  					<div class="w3agile_flickr_post_left">
+  						<a href="single.html"><img src="images/2.jpg" alt=" " class="img-responsive" /></a>
+  					</div>
+  					<div class="w3agile_flickr_post_left">
+  						<a href="single.html"><img src="images/6.jpg" alt=" " class="img-responsive" /></a>
+  					</div>
+  					<div class="w3agile_flickr_post_left">
+  						<a href="single.html"><img src="images/5.jpg" alt=" " class="img-responsive" /></a>
+  					</div>
+  					<div class="clearfix"> </div>
+  				</div>
+  			</div>
+  			<div class="clearfix"> </div>
+  		</div>
+  	</div>
+  <!-- //single -->
+@endsection
