843e2d42e2633189da2183ad304069844a4bea0c
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-09 20:31:54 +0700

    Change to posts

--- database/migrations/2017_05_09_072901_create_posts_table.php
+++ database/migrations/2017_05_09_072901_create_posts_table.php
@@ -13,7 +13,7 @@ class CreateBlogsTable extends Migration
      */
     public function up()
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
             $table->text('content');
