d3ca291070ac3594aa8b12ff6dd321f9d0674451
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-10 15:33:00 +0700

    Update

--- database/migrations/2017_05_10_081243_create_post_tag_table.php
+++ database/migrations/2017_05_10_081243_create_post_tag_table.php
@@ -13,7 +13,13 @@ class CreatePostTagTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->integer('tag_id')->unsigned();
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
     }
 
     /**
@@ -23,6 +29,6 @@ class CreatePostTagTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('post_tag');
     }
 }
