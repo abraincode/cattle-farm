aff733ec6e82e81cf108ba6df132c21506d1e3d0
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-10 16:04:23 +0700

    Update

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    @include('partials._head')
+    @include('dashboard.layouts.partials._head')
   </head>
 
   <body>
 
-    @include('partials._nav')
+    @include('dashboard.layouts.partials._nav')
 
     <div class="container">
-      @include('partials._messages')
+      @include('dashboard.layouts.partials._messages')
 
       @yield('content')
 
-      @include('partials._footer')
+      @include('dashboard.layouts.partials._footer')
 
     </div> <!-- end of .container -->
 
-        @include('partials._javascript')
+        @include('dashboard.layouts.partials._javascript')
 
         @yield('scripts')
 
