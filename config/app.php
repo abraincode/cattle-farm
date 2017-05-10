835ca60de861f985b6bec5d0a731418cd918f8b5
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-11 06:09:29 +0700

    Add intervention image

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -229,7 +230,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Input' => Illuminate\Support\Facades\Input::class,
-
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
