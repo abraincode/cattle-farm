20ac6b82608cfd3ca940611e3e879ce926660a5f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-11 08:52:13 +0700

    Update with image & purifier

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Tag;
 use App\Category;
 use Session;
+use Purifier;
+use Image;
 
 class PostController extends Controller
 {
@@ -46,32 +48,37 @@ class PostController extends Controller
         // validasi data
         $this->validate($request, array(
                 'title'         => 'required|max:255',
-                'slug'          => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
+                //'slug'          => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
                 'category_id'   => 'required|integer',
-                'body'          => 'required'
+                'content'          => 'required'
 
             ));
 
-        // simpan ke database request tertentu
-        /*
-        $post = new Mahasiswa;
-        $post->nim = $request->nim;
-        $post->nama = $request->nama;
-        $post->alamat = $request->alamat;
-        $post->telepon = $request->telepon;
-        $post->jenis_kelamin = $request->jenis_kelamin;
-        $post->agama = $request->agama;
-        $post->save();
-        */
+        // store in the database
+        $post = new Post;
+        $post->title = $request->title;
+        $post->slug = str_slug($post->title);
+        //$post->slug = $request->slug;
+        $post->category_id = $request->category_id;
+        $post->content = Purifier::clean($request->content);
 
-        // simpan ke database semua request
-        \App\Post::create($request->all());
+        if ($request->hasFile('featured_img')) {
+          $image = $request->file('featured_img');
+          $filename = time() . '.' . $image->getClientOriginalExtension();
+          $location = public_path('images/' . $filename);
+          Image::make($image)->resize(800, 400)->save($location);
+          $post->image = $filename;
+        }
+
+        $post->save();
+
+        $post->tags()->sync($request->tags, false);
 
         // flash messages
         $request->session()->flash('status', 'Data berhasil disimpan!');
 
         // redirect ke halaman
-        return redirect()->route('admin.post.index');
+        return redirect()->route('admin.post.index', $post->id);
     }
 
     /**
@@ -83,7 +90,14 @@ class PostController extends Controller
     public function show($id)
     {
         $post = Post::findOrFail($id);
-        return view('dashboard.post.show', compact('blog'));
+        return view('dashboard.posts.show', compact('post'));
+    }
+
+    public function showSlug($slug)
+    {
+        //fetch from the DB based on slug
+        $post = Post::where('slug', '=',  $slug)->first();
+        return view('dashboard.posts.show', compact('post'));
     }
 
     /**
@@ -94,8 +108,22 @@ class PostController extends Controller
      */
     public function edit($id)
     {
+        // find the post in the database and save as a var
         $post = Post::findOrFail($id);
-        return view('dashboard.posts.edit', compact('blog'));
+        $categories = Category::all();
+        $cats = array();
+        foreach ($categories as $category) {
+            $cats[$category->id] = $category->name;
+        }
+
+        $tags = Tag::all();
+        $tags2 = array();
+        foreach ($tags as $tag) {
+            $tags2[$tag->id] = $tag->name;
+        }
+        // return the view and pass in the var we previously created
+        //return view('posts.edit')->withPost($post)->withCategories($cats)->withTags($tags2);
+        return view('dashboard.posts.edit', compact('blog','cats','tags2'));
     }
 
     /**
@@ -107,9 +135,43 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
+      // Validate the data
         $post = Post::findOrFail($id);
-        $post->update($request->all());
-        return redirect()->route('admin.post.index')->with('status','Data berhasil diperbarui.');
+
+        if ($request->input('slug') == $post->slug) {
+            $this->validate($request, array(
+                'title' => 'required|max:255',
+                'category_id' => 'required|integer',
+                'content'  => 'required'
+            ));
+        } else {
+        $this->validate($request, array(
+                'title' => 'required|max:255',
+                'slug'  => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
+                'category_id' => 'required|integer',
+                'content'  => 'required'
+            ));
+        }
+
+        // Save the data to the database
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->slug = $request->input('slug');
+        $post->category_id = $request->input('category_id');
+        $post->content = Purifier::clean($request->input('content'));
+
+        $post->save();
+
+        if (isset($request->tags)) {
+            $post->tags()->sync($request->tags);
+        } else {
+            $post->tags()->sync(array());
+        }
+
+        // set flash data with success message
+        //Session::flash('success', 'This post was successfully saved.');
+        // redirect with flash data to posts.show
+        return redirect()->route('admin.post.index', $post->id)->with('status','Data berhasil diperbarui.');
     }
 
     /**
@@ -120,7 +182,10 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        $post = Post::findOrFail($id)->delete();
+        $post = Post::findOrFail($id);
+        $post->tags()->detach();
+        $post->delete();
+        //Session::flash('success', 'The post was successfully deleted.');
         return redirect()->route('admin.post.index')->with('status','Data berhasil dihapus.');
     }
 }
